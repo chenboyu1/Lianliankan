feae5b5fd8123a6a61ddcecedb1718fea7e87a12
陳柏妤 <147235033+chenboyu1@users.noreply.github.com>
2024-05-23 11:53:43 +0800

    Delete Direction.ts

--- Direction.ts
+++
@@ -1,6 +0,0 @@
-export class Direction {
-    public static UP: string = "up";
-    public static DOWN: string = "down";
-    public static RIGHT: string = "right";
-    public static LEFT: string = "left";
-}
